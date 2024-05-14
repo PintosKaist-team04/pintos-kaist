0efc7a509981cd060ee55e14d769bd33571731db
kimjooyoung <jujulia3040@gmail.com>
2024-05-14 14:03:54 +0000

    comment: filesys 주석 번역 추가

--- include/filesys/filesys.h
+++ include/filesys/filesys.h
@@ -4,10 +4,12 @@
 #include <stdbool.h>
 #include "filesys/off_t.h"
 
+/* 시스템 파일 아이노드의 섹터 */
 /* Sectors of system file inodes. */
-#define FREE_MAP_SECTOR 0       /* Free map file inode sector. */
-#define ROOT_DIR_SECTOR 1       /* Root directory file inode sector. */
+#define FREE_MAP_SECTOR 0       /* 빈 맵 파일 아이노드 섹터 / Free map file inode sector. */
+#define ROOT_DIR_SECTOR 1       /* 루트 디렉터리 파일 아이노드 섹터 / Root directory file inode sector. */
 
+/* 파일 시스템에 사용되는 디스크 */
 /* Disk used for file system. */
 extern struct disk *filesys_disk;
 
