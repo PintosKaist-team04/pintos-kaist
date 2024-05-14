c1c52a9d3ff9da5d3c964ed92ddba128213086ff
makeit-jy <makeit_jy@naver.com>
2024-05-14 22:46:28 +0900

    comment: vm 주석 번역 추가

--- vm/inspect.c
+++ vm/inspect.c
@@ -1,3 +1,5 @@
+/* inspect.c: VM을 위한 테스팅 유틸리티. */
+/* 이 파일을 수정하지 마세요. */
 /* inspect.c: Testing utility for VM. */
 /* DO NOT MODIFY THIS FILE. */
 
@@ -12,6 +14,11 @@ inspect (struct intr_frame *f) {
 	f->R.rax = PTE_ADDR (pml4_get_page (thread_current ()->pml4, va));
 }
 
+/* VM 구성 요소를 테스트하기 위한 도구. 이 함수를 int 0x42를 통해 호출합니다.
+ * 입력:
+ *   @RAX - 조사할 가상 주소
+ * 출력:
+ *   @RAX - 입력에 매핑된 물리 주소. */
 /* Tool for testing vm component. Calling this function via int 0x42.
  * Input:
  *   @RAX - Virtual address to inspect
