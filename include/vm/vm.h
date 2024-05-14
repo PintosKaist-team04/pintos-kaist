34def616a3b31a2dd303e65f30d4a5384dbb6927
makeit-jy <makeit_jy@naver.com>
2024-05-14 22:46:28 +0900

    vm 관련 주석 translation

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -4,22 +4,30 @@
 #include "threads/palloc.h"
 
 enum vm_type {
+	/* 페이지가 초기화되지 않음 */
 	/* page not initialized */
 	VM_UNINIT = 0,
+	/* 파일과 관련 없는 페이지, 일명 익명 페이지 */
 	/* page not related to the file, aka anonymous page */
-	VM_ANON = 1,
+	VM_ANON = 1,	
+	/* 파일과 관련된 페이지 */
 	/* page that realated to the file */
-	VM_FILE = 2,
+	VM_FILE = 2,	
+	/* 프로젝트 4의 페이지 캐시를 보유하는 페이지 */
 	/* page that hold the page cache, for project 4 */
 	VM_PAGE_CACHE = 3,
 
+	
+	/* 상태를 저장하는 비트 플래그 */
 	/* Bit flags to store state */
 
+	/* 정보를 저장하는 보조 비트 플래그 표시기입니다. int에 값이 맞을 때까지 더 많은 표시기를 추가할 수 있습니다. */
 	/* Auxillary bit flag marker for store information. You can add more
 	 * markers, until the value is fit in the int. */
 	VM_MARKER_0 = (1 << 3),
 	VM_MARKER_1 = (1 << 4),
 
+	/* 이 값 이상으로는 초과하지 마십시오. */
 	/* DO NOT EXCEED THIS VALUE. */
 	VM_MARKER_END = (1 << 31),
 };
@@ -36,17 +44,23 @@ struct thread;
 
 #define VM_TYPE(type) ((type) & 7)
 
+/* "페이지"의 표현입니다.
+ * 이는 네 개의 "자식 클래스"인 uninit_page, file_page, anon_page 및 페이지 캐시 (프로젝트 4)를 가진 "부모 클래스"의 종류입니다.
+ * 이 구조체의 사전 정의된 멤버를 제거하거나 수정하지 마십시오. */
 /* The representation of "page".
  * This is kind of "parent class", which has four "child class"es, which are
  * uninit_page, file_page, anon_page, and page cache (project4).
  * DO NOT REMOVE/MODIFY PREDEFINED MEMBER OF THIS STRUCTURE. */
 struct page {
 	const struct page_operations *operations;
-	void *va;              /* Address in terms of user space */
-	struct frame *frame;   /* Back reference for frame */
+	void *va;              /* Address in terms of user space */ /* 사용자 공간에서의 주소 */
+	struct frame *frame;   /* Back reference for frame */ /* 프레임에 대한 역 참조 */
 
-	/* Your implementation */
+	/* Your implementation */ /* 여러분의 구현 */
 
+
+	/* 각 유형의 데이터는 연합체에 바인딩됩니다.
+	 * 각 함수는 자동으로 현재의 연합체를 감지합니다. */
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
 	union {
@@ -59,12 +73,17 @@ struct page {
 	};
 };
 
+/* "프레임"의 표현 */
 /* The representation of "frame" */
 struct frame {
 	void *kva;
 	struct page *page;
 };
 
+/* 페이지 작업을 위한 함수 테이블입니다.
+ * 이것은 C에서 "인터페이스"를 구현하는 한 가지 방법입니다.
+ * "메소드"의 테이블을 구조체 멤버로 넣고,
+ * 필요할 때마다 호출하십시오. */
 /* The function table for page operations.
  * This is one way of implementing "interface" in C.
  * Put the table of "method" into the struct's member, and
@@ -81,6 +100,9 @@ struct page_operations {
 #define destroy(page) \
 	if ((page)->operations->destroy) (page)->operations->destroy (page)
 
+/* 현재 프로세스의 메모리 공간 표현입니다.
+ * 이 구조체에 대해 특정한 디자인을 강요하고 싶지 않습니다.
+ * 이 구조체에 대한 모든 디자인은 여러분에게 달려 있습니다. */
 /* Representation of current process's memory space.
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
--- vm/anon.c
+++ vm/anon.c
@@ -1,14 +1,17 @@
+/* anon.c: 디스크 이미지가 아닌 페이지(일명 익명 페이지)의 구현. */
 /* anon.c: Implementation of page for non-disk image (a.k.a. anonymous page). */
 
 #include "vm/vm.h"
 #include "devices/disk.h"
 
+/* 아래 줄을 수정하지 마세요 */
 /* DO NOT MODIFY BELOW LINE */
 static struct disk *swap_disk;
 static bool anon_swap_in (struct page *page, void *kva);
 static bool anon_swap_out (struct page *page);
 static void anon_destroy (struct page *page);
 
+/* 이 구조체를 수정하지 마세요 */
 /* DO NOT MODIFY this struct */
 static const struct page_operations anon_ops = {
 	.swap_in = anon_swap_in,
@@ -17,34 +20,41 @@ static const struct page_operations anon_ops = {
 	.type = VM_ANON,
 };
 
+/* 익명 페이지를 위한 데이터 초기화 */
 /* Initialize the data for anonymous pages */
 void
 vm_anon_init (void) {
+	/* 할 일: 스왑 디스크를 설정하세요. */
 	/* TODO: Set up the swap_disk. */
 	swap_disk = NULL;
 }
 
+/* 파일 매핑 초기화 */
 /* Initialize the file mapping */
 bool
 anon_initializer (struct page *page, enum vm_type type, void *kva) {
+	/* 핸들러 설정 */
 	/* Set up the handler */
 	page->operations = &anon_ops;
 
 	struct anon_page *anon_page = &page->anon;
 }
 
+/* 스왑 디스크에서 내용을 읽어 페이지를 스왑 인합니다. */
 /* Swap in the page by read contents from the swap disk. */
 static bool
 anon_swap_in (struct page *page, void *kva) {
 	struct anon_page *anon_page = &page->anon;
 }
 
+/* 내용을 스왑 디스크에 쓰면서 페이지를 스왑 아웃합니다. */
 /* Swap out the page by writing contents to the swap disk. */
 static bool
 anon_swap_out (struct page *page) {
 	struct anon_page *anon_page = &page->anon;
 }
 
+/* 익명 페이지를 파괴합니다. PAGE는 호출자에 의해 해제될 것입니다. */
 /* Destroy the anonymous page. PAGE will be freed by the caller. */
 static void
 anon_destroy (struct page *page) {
